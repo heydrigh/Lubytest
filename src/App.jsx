305adcd6ac01e53df84fb92ee1783b8015e936b1
heydrigh <heydrigh@gmail.com>
2020-11-23 20:37:02 -0300

    Creating basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
+const App = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
