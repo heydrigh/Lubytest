f38bb0ba72c6918ad837c8fa0b0a911ecc88a494
heydrigh <heydrigh@gmail.com>
2020-11-25 17:53:56 -0300

    adding fonts

--- src/styles/global.js
+++ src/styles/global.js
@@ -7,11 +7,20 @@ export default createGlobalStyle`
     outline: 0;
     box-sizing: border-box;
   }
+  @font-face {
+  font-family: 'HelveticaNeue';
+  src: local('HelveticaNeue'), url(./assets/fonts/HelveticaNeue.ttf) format('truetype');}
+
+  @font-face {
+  font-family: 'HelveticaNeueBold';
+  src: local('HelveticaNeueBold'), url(./assets/fonts/HelveticaNeueBD.ttf) format('truetype');}
 
   body {
     background: #292929;
-    overflow-x: hidden
+    overflow-x: hidden;
+    font-family: 'HelveticaNeue', Arial, Helvetica, sans-serif;
   }
+
 
   button {
     cursor: pointer;
